c040fdda9afb70d5e288d62ecf4e062b374eb169
zack_zhu <zack.zhu@daqri.com>
2016-09-01 15:08:22 -0700

    update

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>Zack First Angular 2 App</h1>'
+  template: '<h1>Zack Second Angular 2 App</h1>'
 })
 export class AppComponent { }
